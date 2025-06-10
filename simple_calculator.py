e118a783e40dafd895b1f76726e937f3dcb6131d
Jackson Blackman <jacksonblackman@Jacksons-MacBook-Pro.local>
2025-06-10 16:21:35 -0400

    Uploaded file

---
+++ simple_calculator.py
@@ -0,0 +1,18 @@
+# Jackson Blackman
+# May 16th, 2025
+# Calculator program
+
+while True:
+    operation = input("What is your operation?:")
+    numberOne = int(input("What is your first number?:"))
+    numberTwo = int(input("What is your second number?:"))
+    if operation == "*":
+        answer = numberOne * numberTwo
+    elif operation == "+":
+        answer = numberTwo + numberOne
+    elif operation == "-":
+        answer = numberOne - numberTwo
+    elif operation == "%":
+        answer = numberOne / numberTwo
+    print(numberOne, operation, numberTwo, " = ", answer )
+    
